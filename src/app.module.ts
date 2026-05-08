24e64c2b5ebb774dfa9bbc3aa2f4b1db03a6e6d2
Salvador Nicolas <snicoper@gmail.com>
2026-05-08 21:48:01 +0200

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DriversModule } from './drivers/drivers.module';
+
+@Module({
+  imports: [DriversModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/drivers/drivers.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class DriversService {
+  private readonly drivers = [
+    { id: 1, name: 'Salva', nationality: 'Spain' },
+    { id: 2, name: 'Max Verstappen', nationality: 'Netherlands' },
+    { id: 3, name: 'Fernando Alonso', nationality: 'Spain' },
+  ];
+
+  findAll() {
+    return this.drivers;
+  }
+
+  findOne(id: number) {
+    return this.drivers.find((driver) => driver.id === id);
+  }
+}

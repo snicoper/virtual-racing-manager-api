202746cff4d621e6a059f1a9194ca4520fecde08
Salvador Nicolas <snicoper@gmail.com>
2026-05-10 09:40:11 +0200

    cleanup app bootstrap and improve auth structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { DriversModule } from './drivers/drivers.module';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [PrismaModule, DriversModule, UsersModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
